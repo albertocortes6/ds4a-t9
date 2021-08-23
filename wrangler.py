5c684653e50c71e898125b36f873839d2946a41b
albertocortes6 <87874024+albertocortes6@users.noreply.github.com>
2021-08-23 14:46:18 -0500

    Update wrangler.py

--- wrangler.py
+++ wrangler.py
@@ -1,1 +1,13 @@
+#Base de datos 2
 
+def normalize_column_names_db2(df):
+    
+    df2 = df.copy()
+    original_names = list(df2.columns)
+    normalized_names = [x.upper() for x in original_names]
+    
+    df2.columns = normalized_names
+    
+    return df2
+
+df_indicadores_turismo = normalize_column_names_db2(df_indicadores_turismo)
